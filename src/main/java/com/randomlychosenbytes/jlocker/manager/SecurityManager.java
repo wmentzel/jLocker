38052b4b9a76919366bd31fc86b4d54bd5c86152
Willi Mentzel <willi.mentzel@gmail.com>
2019-12-29 14:13:48 +0100

    refactoring

--- src/main/java/com/randomlychosenbytes/jlocker/manager/SecurityManager.java
+++ src/main/java/com/randomlychosenbytes/jlocker/manager/SecurityManager.java
@@ -6,6 +6,7 @@ import com.google.gson.reflect.TypeToken;
 import com.randomlychosenbytes.jlocker.nonabstractreps.Building;
 
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
@@ -21,6 +22,9 @@ import java.util.List;
  * decryption.
  */
 final public class SecurityManager {
+
+    private static final String cryptoAlgorithmName = "DES";
+
     /**
      * Retuns a MD5 hash to a given array of bytes.
      */
@@ -46,15 +50,15 @@ final public class SecurityManager {
     }
 
     public static byte[] encrypt(String s, SecretKey key) throws Exception {
-        Cipher ecipher = Cipher.getInstance("DES");
+        Cipher ecipher = Cipher.getInstance(cryptoAlgorithmName);
         ecipher.init(Cipher.ENCRYPT_MODE, key);
-        return ecipher.doFinal(s.getBytes("UTF8"));
+        return ecipher.doFinal(s.getBytes());
     }
 
     public static String decrypt(byte[] bytes, SecretKey key) throws Exception {
-        Cipher dcipher = Cipher.getInstance("DES");
+        Cipher dcipher = Cipher.getInstance(cryptoAlgorithmName);
         dcipher.init(Cipher.DECRYPT_MODE, key);
-        return new String(dcipher.doFinal(bytes), "UTF8");
+        return new String(dcipher.doFinal(bytes));
     }
 
     /**
@@ -70,12 +74,16 @@ final public class SecurityManager {
         }.getType());
     }
 
+    public static String generateAndEncryptKey(String pw) throws NoSuchAlgorithmException {
+        return encryptKeyWithString(KeyGenerator.getInstance(cryptoAlgorithmName).generateKey(), pw);
+    }
+
     public static String encryptKeyWithString(SecretKey key, String pw) {
         try {
-            Cipher ecipher = Cipher.getInstance("DES");
+            Cipher ecipher = Cipher.getInstance(cryptoAlgorithmName);
 
             DESKeySpec desKeySpec = new DESKeySpec(pw.getBytes());
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(cryptoAlgorithmName);
             SecretKey secretKey = keyFactory.generateSecret(desKeySpec);
             ecipher.init(Cipher.ENCRYPT_MODE, secretKey);
 
@@ -89,14 +97,14 @@ final public class SecurityManager {
 
     public static SecretKey decryptKeyWithString(String encKeyBase64, String pw) { // Key is saved as string
         try {
-            Cipher dcipher = Cipher.getInstance("DES");
+            Cipher dcipher = Cipher.getInstance(cryptoAlgorithmName);
 
             DESKeySpec desKeySpec = new DESKeySpec(pw.getBytes());
-            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(cryptoAlgorithmName);
             SecretKey secretKey = keyFactory.generateSecret(desKeySpec);
             dcipher.init(Cipher.DECRYPT_MODE, secretKey);
 
-            return new SecretKeySpec(dcipher.doFinal(base64StringToBytes(encKeyBase64)), "DES");
+            return new SecretKeySpec(dcipher.doFinal(base64StringToBytes(encKeyBase64)), cryptoAlgorithmName);
         } catch (Exception e) {
             System.err.println("* User.DecryptKeyWithString()... failed");
         }
--- src/main/java/com/randomlychosenbytes/jlocker/nonabstractreps/SuperUser.java
+++ src/main/java/com/randomlychosenbytes/jlocker/nonabstractreps/SuperUser.java
@@ -3,11 +3,10 @@ package com.randomlychosenbytes.jlocker.nonabstractreps;
 import com.google.gson.annotations.Expose;
 import com.randomlychosenbytes.jlocker.manager.SecurityManager;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import java.security.NoSuchAlgorithmException;
 
-import static com.randomlychosenbytes.jlocker.manager.SecurityManager.encryptKeyWithString;
+import static com.randomlychosenbytes.jlocker.manager.SecurityManager.generateAndEncryptKey;
 
 public class SuperUser extends User {
 
@@ -25,11 +24,11 @@ public class SuperUser extends User {
         try {
             // everyone has at least this password.
             // it's used to encrypt/decrypt the buildings object
-            encUserMasterKeyBase64 = encryptKeyWithString(KeyGenerator.getInstance("DES").generateKey(), decUserPW);
+            encUserMasterKeyBase64 = generateAndEncryptKey(decUserPW);
 
             // only super users have this variables initialized
             // this key is used to encrypt/decrypt the locker codes
-            encSuperUMasterKeyBase64 = encryptKeyWithString(KeyGenerator.getInstance("DES").generateKey(), decUserPW);
+            encSuperUMasterKeyBase64 = generateAndEncryptKey(decUserPW);
         } catch (NoSuchAlgorithmException ex) {
             System.out.println("*** Executing User Constructor... failed");
         }
