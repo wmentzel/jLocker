98aca9f495c6ddf7e7be503e26d2b8c94d0b0b32
Willi Mentzel <willi.mentzel@gmail.com>
2020-12-14 21:09:45 +0100

    convert EntityCoordinates to Kotlin

--- src/main/java/com/randomlychosenbytes/jlocker/abstractreps/EntityCoordinates.kt
+++ src/main/java/com/randomlychosenbytes/jlocker/abstractreps/EntityCoordinates.kt
@@ -1,4 +1,4 @@
-package com.randomlychosenbytes.jlocker.abstractreps;
+package com.randomlychosenbytes.jlocker.abstractreps
 
 /**
  * Every Entity (ManamgentUnit, Locker, Room etc.) has a unique sequence of numbers.
@@ -9,68 +9,11 @@ package com.randomlychosenbytes.jlocker.abstractreps;
  * (1st coordinate), floor (2nd coordinate) and walk (3rd coordinate) the locker
  * is in.
  */
-public class EntityCoordinates<T> {
-    T obj;
-    int b, f, w, m, l;
-
-    public EntityCoordinates(T t, int b, int f, int w, int m, int l) {
-        obj = t;
-        this.b = b;
-        this.f = f;
-        this.w = w;
-        this.m = m;
-        this.l = l;
-    }
-
-    public EntityCoordinates(int b, int f, int w, int m, int l) {
-        this.b = b;
-        this.f = f;
-        this.w = w;
-        this.m = m;
-        this.l = l;
-    }
-
-    public int getBValue() {
-        return b;
-    }
-
-    public int getFValue() {
-        return f;
-    }
-
-    public int getWValue() {
-        return w;
-    }
-
-    public int getMValue() {
-        return m;
-    }
-
-    public int getLValue() {
-        return l;
-    }
-
-    public T getEntity() {
-        return obj;
-    }
-
-    public void setBValue(int b) {
-        this.b = b;
-    }
-
-    public void setFValue(int f) {
-        this.f = f;
-    }
-
-    public void setWValue(int w) {
-        this.w = w;
-    }
-
-    public void setMValue(int m) {
-        this.m = m;
-    }
-
-    public void setLValue(int l) {
-        this.l = l;
-    }
-}
+class EntityCoordinates<T : Any>(
+    var entity: T,
+    var bValue: Int,
+    var fValue: Int,
+    var wValue: Int,
+    var mValue: Int,
+    var lValue: Int,
+)
