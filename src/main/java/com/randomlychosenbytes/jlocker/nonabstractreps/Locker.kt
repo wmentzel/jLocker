7e702422dedbf2c8c1c2ecb519353cd516af0ef6
Willi Mentzel <willi.mentzel@gmail.com>
2020-12-14 22:02:56 +0100

    convert to Kotlin

--- src/main/java/com/randomlychosenbytes/jlocker/nonabstractreps/Locker.kt
+++ src/main/java/com/randomlychosenbytes/jlocker/nonabstractreps/Locker.kt
@@ -1,412 +1,280 @@
-package com.randomlychosenbytes.jlocker.nonabstractreps;
+package com.randomlychosenbytes.jlocker.nonabstractreps
 
-import com.google.gson.annotations.Expose;
-import com.randomlychosenbytes.jlocker.manager.DataManager;
+import com.google.gson.annotations.Expose
+import com.randomlychosenbytes.jlocker.manager.DataManager
+import com.randomlychosenbytes.jlocker.manager.Utils
+import java.awt.Color
+import java.awt.Font
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.util.*
+import javax.crypto.SecretKey
+import javax.swing.JLabel
 
-import javax.crypto.SecretKey;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
-import static com.randomlychosenbytes.jlocker.manager.Utils.*;
-
-public class Locker extends JLabel implements Cloneable {
-
-    @Expose
-    private String id;
-
-    @Expose
-    private String lastName;
-
-    @Expose
-    private String firstName;
-
-    @Expose
-    private int heightInCm;
-
-    @Expose
-    private String schoolClassName;
-
-    @Expose
-    private String rentedFromDate; // TODO: use LocalDate
+class Locker(
+    @field:Expose private var id: String,
+    @field:Expose var ownerName: String,
+    @field:Expose var lastName: String,
+    @field:Expose var ownerSize: Int,
+    @field:Expose var ownerClass: String, // TODO: use LocalDate
+    @field:Expose var rentedFromDate: String, // TODO: use LocalDate
+    @field:Expose var rentedUntilDate: String,
+    @field:Expose private var hasContract: Boolean,
+    @field:Expose var paidAmount: Int,
+    @field:Expose var previouslyPaidAmount: Int,
+    currentCodeIndex: Int,
+    encryptedCodes: Array<String?>?,
+    lockCode: String,
+    isOutOfOrder: Boolean,
+    note: String
+) : JLabel(), Cloneable {
 
     @Expose
-    private String rentedUntilDate; // TODO: use LocalDate
+    var isOutOfOrder: Boolean
 
     @Expose
-    private boolean hasContract;
+    var lockCode: String
 
     @Expose
-    private int paidAmount;
+    var note: String
 
     @Expose
-    private int previoulyPaidAmount;
+    var currentCodeIndex: Int
 
     @Expose
-    private boolean isOutOfOrder;
-
-    @Expose
-    private String lockCode;
-
-    @Expose
-    private String note;
-
-    @Expose
-    private int currentCodeIndex;
-
-    @Expose
-    private String encryptedCodes[];
-
-    public String[] getEncryptedCodes() {
-        return encryptedCodes;
-    }
+    var encryptedCodes: Array<String?>?
+        private set
 
     // transient
-    private Boolean isSelected = false;
+    var isSelected = false
+        private set
 
-    public Locker(
-            String id, String firstName, String lastName, int heightInCm,
-            String schoolClass, String rentedFrom, String rentedUntil, boolean hasContract,
-            int paidAmount, int previoulyPaidAmount, int currentCodeIndex, String[] encryptedCodes, String lockCode,
-            boolean isOutOfOrder, String note
-    ) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.heightInCm = heightInCm;
-        this.schoolClassName = schoolClass;
-        this.rentedFromDate = rentedFrom;
-        this.rentedUntilDate = rentedUntil;
-        this.hasContract = hasContract;
-        this.paidAmount = paidAmount;
-        this.previoulyPaidAmount = previoulyPaidAmount;
-        this.previoulyPaidAmount = paidAmount;
-        this.isOutOfOrder = isOutOfOrder;
-        this.lockCode = lockCode;
-        this.note = note;
-        this.isSelected = false;
-
-        this.currentCodeIndex = currentCodeIndex;
-        this.encryptedCodes = encryptedCodes;
-
-        // standard color
-        setColor(FREE_COLOR);
-
-        setText(this.id);
-
-        // If true the component paints every pixel within its bounds.
-        setOpaque(true);
-
-        // change font      
-        setFont(new Font(Font.DIALOG, Font.BOLD, 20));
-
-        // font aligment
-        setHorizontalAlignment(SwingConstants.CENTER);
-
-        // assign mouse events
-        setUpMouseListener();
-    }
-
-    public String[] getCodes(SecretKey sukey) {
-        String[] decCodes = new String[5];
-
-        for (int i = 0; i < 5; i++) {
-            decCodes[i] = getCode(i, sukey);
+    fun getCodes(sukey: SecretKey): Array<String?> {
+        val decCodes = arrayOfNulls<String>(5)
+        for (i in 0..4) {
+            decCodes[i] = getCode(i, sukey)
         }
-
-        return decCodes;
+        return decCodes
     }
 
-    private String getCode(int i, SecretKey sukey) {
+    private fun getCode(i: Int, sukey: SecretKey): String {
         if (encryptedCodes == null) {
-            return "00-00-00";
+            return "00-00-00"
         }
-
-        String code = decrypt(encryptedCodes[i], sukey);
-        return code.substring(0, 2) + "-" + code.substring(2, 4) + "-" + code.substring(4, 6);
+        val code = Utils.decrypt(encryptedCodes!![i], sukey)
+        return code.substring(0, 2) + "-" + code.substring(2, 4) + "-" + code.substring(4, 6)
     }
 
-    public void setCodes(String[] codes, SecretKey superUserMasterKey) {
+    fun setCodes(codes: Array<String>, superUserMasterKey: SecretKey?) {
         // codes is unencrypted... encrypting and saving in encCodes
 
         // The Value of code[i] looks like "00-00-00"
         // it is saved without the "-", so we have
         // to remove them.
-
-        encryptedCodes = new String[5];
-
-        for (int i = 0; i < 5; i++) {
-            codes[i] = codes[i].replace("-", "");
+        encryptedCodes = arrayOfNulls(5)
+        for (i in 0..4) {
+            codes[i] = codes[i].replace("-", "")
         }
-
-        for (int i = 0; i < 5; i++) {
-            encryptedCodes[i] = encrypt(codes[i], superUserMasterKey);
+        for (i in 0..4) {
+            encryptedCodes!![i] = Utils.encrypt(codes[i], superUserMasterKey)
         }
     }
 
-    public long getRemainingTimeInMonths() {
-        if (rentedUntilDate.equals("") || rentedFromDate.equals("") || isFree()) {
-            return 0;
+    val remainingTimeInMonths: Long
+        get() {
+            if (rentedUntilDate == "" || rentedFromDate == "" || isFree) {
+                return 0
+            }
+            val today: Calendar = GregorianCalendar()
+            today.isLenient = false
+            today.time
+            val end = Utils.getCalendarFromString(rentedUntilDate)
+            return Utils.getDifferenceInMonths(today, end)
         }
 
-        Calendar today = new GregorianCalendar();
-        today.setLenient(false);
-        today.getTime();
-
-        Calendar end = getCalendarFromString(rentedUntilDate);
-
-        return getDifferenceInMonths(today, end);
-    }
-
-    public void setAppropriateColor() {
+    fun setAppropriateColor() {
         if (hasContract) {
-            setColor(RENTED_COLOR);
+            setColor(RENTED_COLOR)
         } else {
-            setColor(NOCONTRACT_COLOR);
+            setColor(NOCONTRACT_COLOR)
         }
-
-        if (getRemainingTimeInMonths() <= 1) {
-            setColor(ONEMONTHREMAINING_COLOR);
+        if (remainingTimeInMonths <= 1) {
+            setColor(ONEMONTHREMAINING_COLOR)
         }
-
-        if (isFree()) {
-            setColor(FREE_COLOR);
+        if (isFree) {
+            setColor(FREE_COLOR)
         }
-
         if (isOutOfOrder) {
-            setColor(OUTOFORDER_COLOR);
+            setColor(OUTOFORDER_COLOR)
         }
     }
 
-    public void empty() {
-        lastName = "";
-        firstName = "";
-        heightInCm = 0;
-        schoolClassName = "";
-        rentedFromDate = "";
-        rentedUntilDate = "";
-        hasContract = false;
-        paidAmount = 0;
-        previoulyPaidAmount = 0;
-
-        if (encryptedCodes == null) {
-            currentCodeIndex = 0;
+    fun empty() {
+        lastName = ""
+        ownerName = ""
+        ownerSize = 0
+        ownerClass = ""
+        rentedFromDate = ""
+        rentedUntilDate = ""
+        hasContract = false
+        paidAmount = 0
+        previouslyPaidAmount = 0
+        currentCodeIndex = if (encryptedCodes == null) {
+            0
         } else {
-            currentCodeIndex = (currentCodeIndex + 1) % encryptedCodes.length;
+            (currentCodeIndex + 1) % encryptedCodes!!.size
         }
-        setAppropriateColor();
+        setAppropriateColor()
     }
 
-    public void setTo(Locker newdata) {
-        lastName = newdata.lastName;
-        firstName = newdata.firstName;
-        heightInCm = newdata.heightInCm;
-        schoolClassName = newdata.schoolClassName;
-        rentedFromDate = newdata.rentedFromDate;
-        rentedUntilDate = newdata.rentedUntilDate;
-        hasContract = newdata.hasContract;
-        paidAmount = newdata.paidAmount;
-        previoulyPaidAmount = newdata.previoulyPaidAmount;
-
-        setAppropriateColor();
+    fun setTo(newdata: Locker) {
+        lastName = newdata.lastName
+        ownerName = newdata.ownerName
+        ownerSize = newdata.ownerSize
+        ownerClass = newdata.ownerClass
+        rentedFromDate = newdata.rentedFromDate
+        rentedUntilDate = newdata.rentedUntilDate
+        hasContract = newdata.hasContract
+        paidAmount = newdata.paidAmount
+        previouslyPaidAmount = newdata.previouslyPaidAmount
+        setAppropriateColor()
     }
 
-    public void setId(String id) {
-        setText(this.id = id);
+    fun setId(id: String) {
+        text = id.also { this.id = it }
     }
 
-    public void setLastName(String sirname) {
-        lastName = sirname;
+    fun setFirstName(name: String) {
+        ownerName = name
     }
 
-    public void setFirstName(String name) {
-        firstName = name;
+    fun setHeightInCm(size: Int) {
+        ownerSize = size
     }
 
-    public void setHeightInCm(int size) {
-        heightInCm = size;
+    fun setSchoolClass(_class: String) {
+        ownerClass = _class
     }
 
-    public void setSchoolClass(String _class) {
-        schoolClassName = _class;
+    fun setFromDate(fromdate: String) {
+        rentedFromDate = fromdate
     }
 
-    public void setFromDate(String fromdate) {
-        rentedFromDate = fromdate;
+    fun setUntilDate(untildate: String) {
+        rentedUntilDate = untildate
     }
 
-    public void setUntilDate(String untildate) {
-        rentedUntilDate = untildate;
+    fun setHasContract(hascontract: Boolean) {
+        hasContract = hascontract
     }
 
-    public void setHasContract(boolean hascontract) {
-        hasContract = hascontract;
+    fun setNextCode() {
+        currentCodeIndex = (currentCodeIndex + 1) % 5
     }
 
-    public void setPaidAmount(int money) {
-        paidAmount = money;
+    fun setSelected() {
+        isSelected = true
+        setColor(SELECTED_COLOR)
     }
 
-    public void setPreviouslyPaidAmount(int amount) {
-        previoulyPaidAmount = amount;
-    }
-
-    public void setCurrentCodeIndex(int index) {
-        currentCodeIndex = index;
-    }
-
-    public void setNextCode() {
-        currentCodeIndex = (currentCodeIndex + 1) % 5;
-    }
-
-    public void setOutOfOrder(boolean outoforder) {
-        isOutOfOrder = outoforder;
-    }
-
-    public void setSelected() {
-        isSelected = true;
-        setColor(SELECTED_COLOR);
-    }
-
-    public void setLockCode(String lock) {
-        lockCode = lock;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public final void setUpMouseListener() {
-        if (this.getMouseListeners().length == 0) {
-            addMouseListener(new MouseListener());
+    fun setUpMouseListener() {
+        if (this.mouseListeners.size == 0) {
+            addMouseListener(MouseListener())
         }
     }
 
-    public final void setColor(int index) {
-        setBackground(BACKGROUND_COLORS[index]);
-        setForeground(FOREGROUND_COLORS[index]);
+    fun setColor(index: Int) {
+        background = BACKGROUND_COLORS[index]
+        foreground = FOREGROUND_COLORS[index]
     }
 
-    public String getId() {
-        return id;
+    fun getId(): String {
+        return id
     }
 
-    public String getLastName() {
-        return lastName;
+    fun hasContract(): Boolean {
+        return hasContract
     }
 
-    public String getOwnerName() {
-        return firstName;
+    val isFree: Boolean
+        get() = ownerName == ""
+
+    fun getCurrentCode(sukey: SecretKey): String {
+        return getCode(currentCodeIndex, sukey)
     }
 
-    public int getOwnerSize() {
-        return heightInCm;
-    }
-
-    public String getOwnerClass() {
-        return schoolClassName;
-    }
-
-    public String getRentedFromDate() {
-        return rentedFromDate;
-    }
-
-    public String getRentedUntilDate() {
-        return rentedUntilDate;
-    }
-
-    public boolean hasContract() {
-        return hasContract;
-    }
-
-    public int getPaidAmount() {
-        return paidAmount;
-    }
-
-    public int getPreviouslyPaidAmount() {
-        return previoulyPaidAmount;
-    }
-
-    public int getCurrentCodeIndex() {
-        return currentCodeIndex;
-    }
-
-    public boolean isOutOfOrder() {
-        return isOutOfOrder;
-    }
-
-    public String getLockCode() {
-        return lockCode;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public boolean isFree() {
-        return firstName.equals("");
-    }
-
-    public String getCurrentCode(SecretKey sukey) {
-        return getCode(currentCodeIndex, sukey);
-    }
-
-    public Boolean isSelected() {
-        return isSelected;
-    }
-
-    public Locker getCopy() throws CloneNotSupportedException {
-        return (Locker) this.clone();
-    }
-
-    public static final int OUTOFORDER_COLOR = 0;
-    public static final int RENTED_COLOR = 1;
-    public static final int FREE_COLOR = 2;
-    public static final int SELECTED_COLOR = 3;
-    public static final int NOCONTRACT_COLOR = 4;
-    public static final int ONEMONTHREMAINING_COLOR = 5;
-
-    // TODO: use enum
-    private static final Color[] BACKGROUND_COLORS = new Color[]
-            {
-                    new Color(255, 0, 0),
-                    new Color(0, 102, 0),
-                    new Color(255, 255, 255),
-                    new Color(255, 255, 0),
-                    new Color(0, 0, 255),
-                    new Color(255, 153, 0)
-            };
-
-    // TODO: use enum
-    private static final Color[] FOREGROUND_COLORS = new Color[]
-            {
-                    new Color(255, 255, 255),
-                    new Color(255, 255, 255),
-                    new Color(0, 0, 0),
-                    new Color(0, 0, 0),
-                    new Color(255, 255, 255),
-                    new Color(0, 0, 0)
-            };
+    @get:Throws(CloneNotSupportedException::class)
+    val copy: Locker
+        get() = clone() as Locker
 
     /**
      * TODO move to MainFrame
      */
-    private class MouseListener extends MouseAdapter {
-        @Override
-        public void mouseReleased(java.awt.event.MouseEvent e) {
-            DataManager dm = DataManager.getInstance();
-
-            if (dm.getCurLockerList().size() > 0) {
-                dm.getCurLocker().setAppropriateColor();
+    private inner class MouseListener : MouseAdapter() {
+        override fun mouseReleased(e: MouseEvent) {
+            val dm = DataManager.getInstance()
+            if (dm.curLockerList.size > 0) {
+                dm.curLocker.setAppropriateColor()
             }
-
-            Locker locker = (Locker) e.getSource();
-            dm.getCurWalk().setCurLockerIndex(locker);
-
-            locker.setSelected();
-            DataManager.getInstance().getMainFrame().showLockerInformation();
+            val locker = e.source as Locker
+            dm.curWalk.setCurLockerIndex(locker)
+            locker.setSelected()
+            DataManager.getInstance().mainFrame.showLockerInformation()
         }
+    }
+
+    companion object {
+        const val OUTOFORDER_COLOR = 0
+        const val RENTED_COLOR = 1
+        const val FREE_COLOR = 2
+        const val SELECTED_COLOR = 3
+        const val NOCONTRACT_COLOR = 4
+        const val ONEMONTHREMAINING_COLOR = 5
+
+        // TODO: use enum
+        private val BACKGROUND_COLORS = arrayOf(
+            Color(255, 0, 0),
+            Color(0, 102, 0),
+            Color(255, 255, 255),
+            Color(255, 255, 0),
+            Color(0, 0, 255),
+            Color(255, 153, 0)
+        )
+
+        // TODO: use enum
+        private val FOREGROUND_COLORS = arrayOf(
+            Color(255, 255, 255),
+            Color(255, 255, 255),
+            Color(0, 0, 0),
+            Color(0, 0, 0),
+            Color(255, 255, 255),
+            Color(0, 0, 0)
+        )
+    }
+
+    init {
+        previouslyPaidAmount = paidAmount
+        this.isOutOfOrder = isOutOfOrder
+        this.lockCode = lockCode
+        this.note = note
+        isSelected = false
+        this.currentCodeIndex = currentCodeIndex
+        this.encryptedCodes = encryptedCodes
+
+        // standard color
+        setColor(FREE_COLOR)
+        text = id
+
+        // If true the component paints every pixel within its bounds.
+        isOpaque = true
+
+        // change font      
+        font = Font(Font.DIALOG, Font.BOLD, 20)
+
+        // font aligment
+        horizontalAlignment = CENTER
+
+        // assign mouse events
+        setUpMouseListener()
     }
 }
