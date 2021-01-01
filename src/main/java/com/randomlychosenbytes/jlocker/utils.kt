38dfebd8a66652014b6b65a2c32d084504391bae
Willi Mentzel <willi.mentzel@gmail.com>
2021-01-01 21:25:56 +0100

    no more checked exceptions, great!

--- src/main/java/com/randomlychosenbytes/jlocker/utils.kt
+++ src/main/java/com/randomlychosenbytes/jlocker/utils.kt
@@ -19,28 +19,16 @@ import javax.crypto.spec.SecretKeySpec
 
 private const val CRYPTO_ALOGRITHM_NAME = "DES"
 
-private fun getUtf8Bytes(str: String): ByteArray {
-    return try {
-        str.toByteArray(charset("UTF-8"))
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
-}
+private fun getUtf8Bytes(str: String): ByteArray = str.toByteArray(charset("UTF-8"))
 
 /**
  * Retuns a MD5 hash to a given array of bytes.
  */
 fun getHash(pw: String): String {
-    return try {
-        val m = MessageDigest.getInstance("MD5")
-        val bytes = getUtf8Bytes(pw)
-        m.update(bytes, 0, bytes.size)
-        BigInteger(1, m.digest()).toString(16)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
+    val m = MessageDigest.getInstance("MD5")
+    val bytes = getUtf8Bytes(pw)
+    m.update(bytes, 0, bytes.size)
+    return BigInteger(1, m.digest()).toString(16)
 }
 
 private fun bytesToBase64String(bytes: ByteArray): String {
@@ -52,25 +40,16 @@ private fun base64StringToBytes(str: String): ByteArray {
 }
 
 fun encrypt(s: String, key: SecretKey?): String {
-    return try {
-        val ecipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
-        ecipher.init(Cipher.ENCRYPT_MODE, key)
-        bytesToBase64String(ecipher.doFinal(getUtf8Bytes(s)))
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
+    val ecipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
+    ecipher.init(Cipher.ENCRYPT_MODE, key)
+    return bytesToBase64String(ecipher.doFinal(getUtf8Bytes(s)))
+
 }
 
 fun decrypt(base64: String, key: SecretKey?): String {
-    return try {
-        val dcipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
-        dcipher.init(Cipher.DECRYPT_MODE, key)
-        String(dcipher.doFinal(base64StringToBytes(base64)), Charsets.UTF_8)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
+    val dcipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
+    dcipher.init(Cipher.DECRYPT_MODE, key)
+    return String(dcipher.doFinal(base64StringToBytes(base64)), Charsets.UTF_8)
 }
 
 /**
@@ -87,41 +66,26 @@ fun unsealAndDeserializeBuildings(encryptedBuildingsBase64: String, key: SecretK
     return gson.fromJson(json, object : TypeToken<List<Building?>?>() {}.type)
 }
 
-fun generateAndEncryptKey(pw: String): String {
-    return try {
-        encryptKeyWithString(KeyGenerator.getInstance(CRYPTO_ALOGRITHM_NAME).generateKey(), pw)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
-}
+fun generateAndEncryptKey(
+    pw: String
+): String = encryptKeyWithString(KeyGenerator.getInstance(CRYPTO_ALOGRITHM_NAME).generateKey(), pw)
 
 fun encryptKeyWithString(key: SecretKey, pw: String): String {
-    return try {
-        val ecipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
-        val desKeySpec = DESKeySpec(getUtf8Bytes(pw))
-        val keyFactory = SecretKeyFactory.getInstance(CRYPTO_ALOGRITHM_NAME)
-        val secretKey = keyFactory.generateSecret(desKeySpec)
-        ecipher.init(Cipher.ENCRYPT_MODE, secretKey)
-        bytesToBase64String(ecipher.doFinal(key.encoded))
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
+    val ecipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
+    val desKeySpec = DESKeySpec(getUtf8Bytes(pw))
+    val keyFactory = SecretKeyFactory.getInstance(CRYPTO_ALOGRITHM_NAME)
+    val secretKey = keyFactory.generateSecret(desKeySpec)
+    ecipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    return bytesToBase64String(ecipher.doFinal(key.encoded))
 }
 
 fun decryptKeyWithString(encKeyBase64: String, pw: String): SecretKey { // Key is saved as string
-    return try {
-        val dcipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
-        val desKeySpec = DESKeySpec(getUtf8Bytes(pw))
-        val keyFactory = SecretKeyFactory.getInstance(CRYPTO_ALOGRITHM_NAME)
-        val secretKey = keyFactory.generateSecret(desKeySpec)
-        dcipher.init(Cipher.DECRYPT_MODE, secretKey)
-        SecretKeySpec(dcipher.doFinal(base64StringToBytes(encKeyBase64)), CRYPTO_ALOGRITHM_NAME)
-    } catch (e: Exception) {
-        e.printStackTrace()
-        throw RuntimeException()
-    }
+    val dcipher = Cipher.getInstance(CRYPTO_ALOGRITHM_NAME)
+    val desKeySpec = DESKeySpec(getUtf8Bytes(pw))
+    val keyFactory = SecretKeyFactory.getInstance(CRYPTO_ALOGRITHM_NAME)
+    val secretKey = keyFactory.generateSecret(desKeySpec)
+    dcipher.init(Cipher.DECRYPT_MODE, secretKey)
+    return SecretKeySpec(dcipher.doFinal(base64StringToBytes(encKeyBase64)), CRYPTO_ALOGRITHM_NAME)
 }
 
 fun isDateValid(dateStr: String): Boolean = try {
